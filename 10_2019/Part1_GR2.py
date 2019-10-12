122a85916bcbdab2c489f64b6638e7879a42bc39
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-10-12 21:27:27 +0200

    Added lectures

---
+++ 10_2019/Part1_GR2.py
@@ -0,0 +1,378 @@
+# -*- coding: utf-8 -*-
+
+##########
+# PART 1 #
+##########
+
+#---- hello world ----
+#pierwsza linia kodu
+print("Hello WSB :)")
+
+#---- calculator ----
+
+1 + 1
+2 * 10
+5 / 2
+
+5 % 2   #reszta z dzielenia
+
+#---- math / stat functions ----
+
+import numpy as np
+
+np.sum([1, 2, 3])
+np.mean([1,2,3])  #srednia
+np.median([1,2,3])   
+
+#----- variables  ----
+
+a = 1410
+
+abc_Test = "test"
+
+
+#---- data types-----
+
+## logical 
+
+c =True
+d = False
+
+1 == 1
+
+type(c)
+
+## numeric
+
+a = 123
+
+type(a)
+
+b = 1.25
+
+type(b)
+
+
+## text / string
+
+myText = "Python is awesome"
+type(myText)
+
+## Lists 
+
+
+myList = []
+
+myList = [1,2,3, "bla", "bla"]
+
+
+# define a list
+
+
+
+# assign from list
+myList1 = [1,2,3]
+
+a, b, c, = myList1
+
+# add to a list
+
+x = [1,2,3]
+y = [3,4,5]
+
+x += y
+
+# check number of elements
+
+len(x)
+
+# add to a list
+l = []
+
+l.append([3,4,5])
+
+# remove from list 
+l = ['a', 'b', 'c','a', 'b', 'c']
+print(l)
+
+l.remove('a')
+
+print(l)
+
+# select element from list 
+l = ['a', 'b', 'c','a', 'b', 'c']
+l[0]
+l[-1]
+
+## Tuples
+myTuple = ()
+type(myTuple)
+
+myTuple = ('a', 'b')
+myTuple[0] = 'z'
+
+myTuple1 = ('a', 'b','a', 'b')
+print(myTuple1)
+
+myTuple2 = ('a', 'b', 'c')
+print(myTuple2.index('b'))
+
+# nested
+
+myTuple1 = ('a', 'b', 'c')
+myTuple2 = ('d', 'e', 'f')
+sampleTuple = myTuple1, myTuple2
+
+print(sampleTuple)
+sampleTuple
+sampleTuple[1]
+sampleTuple[1][0]
+
+
+## Sets 
+mySet = {'a'}
+print(type(mySet))
+
+print(mySet)
+
+# add new element
+mySet = {'a'}
+mySet.add('a')
+print(mySet)
+
+# check diff
+
+mySet1 = {"a", "b", "c"}
+mySet2 = {"b","c", "d"}
+print(mySet1.difference(mySet2))
+
+# check what is the same 
+
+mySet1 = {"a", "b", "c"}
+mySet2 = {"b","c", "d"}
+print(mySet1.intersection(mySet2))
+
+# check if the whole set is inside another
+mySet1 = {"a", "b", "c"}
+mySet2 = {"a", "b", "c", "d"}
+print(mySet1.issubset(mySet2))
+
+
+
+
+## Dicts
+
+poland_regions = {
+"mazowieckie": 5349114,
+"slaskie": 4570849,
+"wielkopolskie": 3475323
+}
+
+
+
+# add elements
+
+poland_regions['malopolskie'] = 337268
+
+# get all keys 
+
+poland_regions.keys()
+
+tst1 = poland_regions.keys()
+type(tst1)
+
+# get value for requested key
+
+poland_regions.get("slaskie")
+
+# get all values
+
+poland_regions.values()
+
+# delete from dict a value with particular key
+poland_regions= {
+"mazowieckie": 5349114,
+"slaskie": 4570849,
+"wielkopolskie": 3475323
+}
+
+
+
+poland_regions['malopolskie'] = 3372618
+
+poland_regions.pop("malopolskie")
+
+
+## operators 
+
+# relation 
+3 < 4
+3 > 4
+7 <= 7
+0 >= 0
+
+3 == 3.0
+1 == True
+7 == True
+0 == False
+0.5 == True
+1 != -1.0
+
+
+# logical
+
+a = 3 < 4
+b = 3 > 4
+
+a
+b
+
+a&b   #and
+a|b   #lub OR
+
+
+
+
+
+# membership 
+
+pets=['dog','cat','fish']
+
+'fox' in pets
+
+'dog' in pets
+
+
+## loops 
+
+list1 = ['a','b','c']
+
+
+for element in list1:
+    print(element)
+
+for l in list1:
+    print(l)
+
+
+## if clause 
+
+a = 33
+b = 33
+
+if a > b:
+    print("ohh no")
+elif a <=b:
+    print("let it be")
+else:
+    print("Year")
+
+## functions 
+    
+
+def mySum(a,b):
+    result = a + b
+    
+    return(result)
+
+mySum(5, b = 6)
+
+
+## - pandas, numpy: dataframe, array operations
+
+## Numpy package
+# https://docs.scipy.org/doc/numpy/user/quickstart.html
+import numpy as np 
+
+a1 = np.array([1,2,3])
+a2 = np.array([[1,2,3],
+              [4,5,6]])
+
+
+
+
+#to jest tabelaryczne przedstawienie gdzi epodajemy wiersz i kolune  czyli a1[wiersz,kolumna]
+
+# selection 
+
+a1[0]
+a1[1]
+a1[2]
+
+
+a2[0,1]
+
+a2[0:2, 0:1]
+
+# calculations 
+
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+
+x + y
+
+np.add(x, y)
+
+x - y
+
+np.subtract(x,y)
+
+x / y
+
+np.divide(x,y)
+
+
+x = np.array([[1,2],[3,4]])
+
+np.sqrt(x)
+
+x.shape # ile wierszy i kolumn
+
+
+## pandas package
+# https://www.learndatasci.com/tutorials/python-pandas-tutorial-complete-introduction-for-beginners/
+import pandas as pd
+
+# from list
+
+
+# from series 
+
+
+
+# view top rows 
+
+
+# info about data 
+
+
+
+# shape / dimension
+
+
+
+
+# select column from df
+
+
+
+# select columns
+# .loc - locates by name
+# .iloc- locates by numerical index 
+
+# select row from df
+
+
+
+
+
+data3 = [['Gdynia',100],['Gdansk',120],['Sopot',130], ['Gdynia',90], ['Gdansk',100]]
+df3 = pd.DataFrame(data3, columns=['City','Sales'])
+
+# column filters
+
+
+
+# multi axis selection
+
+
---
+++ 10_2019/Part1_ex_GR2.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+
+####################
+# PART 1 exercises #
+####################
+
+
+#--- Exercise 1 -----
+# a) Utwórz zmienną x i przypisz do niej wartość 3.
+# b) Utwórz zmienną y i przypisz do niej wartość 4.0.
+# c) Dodaj obie liczby. Jaki wynik otrzymasz? Jaki jest typ zmiennej wynikowej?
+
+
+x = 3
+
+y = 4.0
+
+z = x+y
+
+#otrzymam float 7.0
+
+type(z)
+
+
+#--- Exercise 2 -----
+# a) Utwórz zmienną x i przypisz do niej wartość 10.0
+# b) Utwórz zmienną y i przypisz do niej wartość 4.0
+# c) Odejmij od zmiennej x zmienną y. Jaki wynik otrzymasz? Jaki jest typ zmiennej wynikowej?
+
+
+x = 10.0
+
+y = 4.0
+
+z = x - y
+
+#otrzymam float 6.0
+
+type(z)
+
+
+#--- Exercise 3 -----
+# a) x = 3.0
+# b) Zmień typ zmiennej x na int.
+
+
+x = 3.0
+
+x = int(x)
+
+print(x, type(x))
+
+
+
+#--- Exercise 4 -----
+# a) x = 3
+# b) zmień type zmiennej na str
+# c) Wydrukuj. Co otrzymasz?
+ 
+x = 3
+
+x = str(x)
+
+print(x, type(x))
+
+
+
+
+
+#--- Exercise 5 -----
+# a. Utwórz zmienną x i przypisz do niej wartość 3.
+# b. Utwórz zmienną y i przypisz do niej wartość 3.0.
+# c. Co wydrukuje x==y?
+# d. Co wydrukuje x==y==True? Dlaczego?
+# e. Co wydrukuje x==y==False? Dlaczego?
+
+
+x = 3
+y = 3.0
+
+print(x==y)
+print(x==y==True)
+print(x==y==False)
+
+
+#--- Exercise 6 -----
+# a. Utwórz zmienną x i przypisz do niej wartość 3.
+# b. Utwórz zmienną y i przypisz do niej wartość 3.
+# c. Utwórz zmienną z i przypisz do niej wartość wyniku operatora porównania zmiennej x i y.
+# d. Używając operatora porównania porównaj z oraz True.
+
+
+x = 3
+y = 3
+z = x==y
+
+print(z==True)
+
+
+
+#--- Exercise 7 -----
+# a. Utwórz zmienną x i przypisz do niej tekst ”3”
+# b. Utwórz zmienną y i przypisz do niej tekst ”4”
+# c. Co wydrukuje x+y?
+
+x = "3"
+y = "4"
+
+print(x+y, type(x+y))
+
+
+#--- Exercise 8 -----
+# a. Utwórz zmienną x i przypisz do niej tekst ”3”
+# b. Utwórz zmienną y i przypisz do niej tekst ”4”
+# c. Dodaj x i y tak, by otrzymać wynik 7 i typ int.
+
+x = "3"
+
+y = "4"
+
+x, y = int(x), int(y)
+
+z = x + y
+
+print(z, type(z))
+
+#--- Exercise 8 -----
+# a. utwórz funkcję, która wydrukuje bilet poprzez połączenie imienia poniższych zmiennych. 
+
+name = "Jan"
+surname = "Kowalski"
+
+def bilet():
+    x = print(name, surname)
+    return x
+
+bilet()
+
+
+def moj(name, surname, sep = " "):
+    bilet = name + sep + surname
+    return bilet
+
+bilet(name=name, syrname = surname)
+
+#--- Exercise 9 -----
+# Utwórz listę o nazwie my_list i dodaj do niej następujące elementy: 1, 3.1, ”a”, False.
+
+my_list = []
+
+my_list = [1, 3.1, "a", False]
+ 
+dane = (1, 3.1, "a", False)
+
+for x in dane:
+    my_list.append(x)
+    
+    
+print(my_list)
+
+
+#--- Exercise 10 -----
+# Wydrukuj drugi element listy.
+my_list2 = [1,2,3]
+
+print(my_list2[1])
+
+
+
+#--- Exercise 11 -----
+#Wydrukuj liczbę elementów w liście my list2.
+
+len(my_list2)
+
+
+
+#--- Exercise 12 -----
+# Wydrukuj pierwsze 3 elementy listy my_list2
+
+print(my_list2[0:3])
+
+
+#--- Exercise 13 -----
+# Dołącz do list1 liste nr 2
+
+nowa_lista = my_list + my_list2
+
+nowa_lista += my_list2
+
+print(nowa_lista)
+
+
+#--- Exercise 14 -----
+# Ile elementów posiada lista?
+
+mylist3 = [[1,2], [3,4], [4,5]]
+
+len(mylist3)
+
+#--- Exercise 15 -----
+# Policz sumę wartosci w pierwszej kolumnie 
+
+input = [
+[1,'asdf'],
+[3, 'dsf'],
+[5, 'dsf'],
+[4, 'dsf'],
+[9, 'dsf'],
+[33, 'dsf'],
+[23, 'dsf']]
+
+
+
+## method 1 - with numpy
+
+input1 = np.array(input)
+x = input1[:, 0]
+
+np.sum(x.astype(int))
+
+
+
+## method 2 - basic method
+
+suma = 0
+
+for x in input:
+    suma += x[0]
+    
+print(suma)
+
+############################
+
+import pandas as pd 
+data_shops = [['Gdynia',100, 'Biedronka'], ['Gdansk',120, 'Biedronka'], ['Sopot',130, 'Lidl'], ['Gdynia',90, 'Lidl'], ['Gdansk',150, 'Kaufland'], ['Sopot', 200, 'Kaufland'], ['Gdansk', 160, 'Lidl'] ]
+df_shops = pd.DataFrame(data_shops, columns=['City','Sales', 'Shops'])
+
+
+#--- Exercise 16 -----
+# Wybierz sklepy Lidl z ramki danych df_shops.
+# Choose Lidl show from df_shops data frame.
+
+
+
+#--- Exercise 17 -----
+# Sprawdz gdzie sprzedaz byla wieksza niz 150 (df_shops).
+# Check where sales is higher than 150 (df_shops).
+
+
+
+#--- Exercise 18 -----
+# Sprawdz w ktorych sklepach w Gdansku i Sopocie sprzedaz przewyzszyla kwote 140. (df_shops)
+# Check in what shops in Gdansk and Sopot sales is higher than 140.
+ 
+
+
+#--- Exercise 19 -----
+# Wybierz wszystkie sklepy oprocz sopockich.
+# Choose all shops except Sopot.
+
+
+
+
+#--- Exercise 20 -----
+# Wybierz wszystkie sklepy Lidl w Gdansku i Sopocie, w ktorych sprzedaz byla wieksza niz 100 oraz mniejsza niz 210
+# Choose all Lidl shops in Gdansk and Sopot where sales is higher than 100 and lower than 210.
+
